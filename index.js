ae739b69f564db064f4f450cfcb34804d3e9000c
SushilSylvester <50085377+SushilSylvester@users.noreply.github.com>
2023-02-21 18:57:54 +0000

    Tidy v3

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ the shader. Apart from that all of the magic happens in the HTML view
 under the fragment shader.
 */
 
+
 let container;
 let camera, scene, renderer;
 let uniforms;
@@ -13,87 +14,90 @@ let uniforms;
 let divisor = 1 / 10;
 
 let newmouse = {
-  x: 0,
-  y: 0
+    x: 0,
+    y: 0
 };
 
-let loader=new THREE.TextureLoader();
+let loader = new THREE.TextureLoader();
 let texture, rtTexture, rtTexture2;
 loader.setCrossOrigin("anonymous");
 loader.load(
-  'https://s3-us-west-2.amazonaws.com/s.cdpn.io/982762/noise.png',
-  function do_something_with_texture(tex) {
-    texture = tex;
-    texture.wrapS = THREE.RepeatWrapping;
-    texture.wrapT = THREE.RepeatWrapping;
-    texture.minFilter = THREE.LinearFilter;
-    init();
-    animate();
-  }
+    'https://s3-us-west-2.amazonaws.com/s.cdpn.io/982762/noise.png',
+    function do_something_with_texture(tex) {
+        texture = tex;
+        texture.wrapS = THREE.RepeatWrapping;
+        texture.wrapT = THREE.RepeatWrapping;
+        texture.minFilter = THREE.LinearFilter;
+        init();
+        animate();
+    }
 );
 
 function init() {
-  container = document.getElementById( 'container' );
+    container = document.getElementById('container');
 
-  camera = new THREE.Camera();
-  camera.position.z = 1;
+    camera = new THREE.Camera();
+    camera.position.z = 1;
 
-  scene = new THREE.Scene();
+    scene = new THREE.Scene();
 
-  var geometry = new THREE.PlaneBufferGeometry( 2, 2 );
-  
-  rtTexture = new THREE.WebGLRenderTarget(window.innerWidth * .2, window.innerHeight * .2);
-  rtTexture2 = new THREE.WebGLRenderTarget(window.innerWidth * .2, window.innerHeight * .2);
+    var geometry = new THREE.PlaneBufferGeometry(2, 2);
 
-  uniforms = {
-    u_time: { type: "f", value: 1.0 },
-    u_resolution: { type: "v2", value: new THREE.Vector2() },
-    u_noise: { type: "t", value: texture },
-    u_buffer: { type: "t", value: rtTexture.texture },
-    u_mouse: { type: "v2", value: new THREE.Vector2() },
-    u_renderpass: { type: 'b', value: false }
-  };
+    rtTexture = new THREE.WebGLRenderTarget(window.innerWidth * .2, window.innerHeight * .2);
+    rtTexture2 = new THREE.WebGLRenderTarget(window.innerWidth * .2, window.innerHeight * .2);
 
-  var material = new THREE.ShaderMaterial( {
-    uniforms: uniforms,
-    vertexShader: document.getElementById( 'vertexShader' ).textContent,
-    fragmentShader: document.getElementById( 'fragmentShader' ).textContent
-  } );
-  material.extensions.derivatives = true;
+    uniforms = {
+        u_time: { type: "f", value: 1.0 },
+        u_resolution: { type: "v2", value: new THREE.Vector2() },
+        u_noise: { type: "t", value: texture },
+        u_buffer: { type: "t", value: rtTexture.texture },
+        u_mouse: { type: "v2", value: new THREE.Vector2() },
+        u_renderpass: { type: 'b', value: false }
+    };
 
-  var mesh = new THREE.Mesh( geometry, material );
-  scene.add( mesh );
+    var material = new THREE.ShaderMaterial({
+        uniforms: uniforms,
+        vertexShader: document.getElementById('vertexShader').textContent,
+        fragmentShader: document.getElementById('fragmentShader').textContent
+    });
+    material.extensions.derivatives = true;
 
-  renderer = new THREE.WebGLRenderer();
-  renderer.setPixelRatio( window.devicePixelRatio );
+    var mesh = new THREE.Mesh(geometry, material);
+    scene.add(mesh);
 
-  container.appendChild( renderer.domElement );
+    renderer = new THREE.WebGLRenderer();
+    renderer.setPixelRatio(window.devicePixelRatio);
 
-  onWindowResize();
-  window.addEventListener( 'resize', onWindowResize, false );
-  
+    container.appendChild(renderer.domElement);
 
-  document.addEventListener('pointermove', (e)=> {
-    let ratio = window.innerHeight / window.innerWidth;
-    newmouse.x = (e.pageX - window.innerWidth / 2) / window.innerWidth / ratio;
-    newmouse.y = (e.pageY - window.innerHeight / 2) / window.innerHeight * -1;
-    
-    e.preventDefault();
-  });
+    onWindowResize();
+    window.addEventListener('resize', onWindowResize, false);
+
+
+    document.addEventListener('pointermove', (e) => {
+        let ratio = window.innerHeight / window.innerWidth;
+        newmouse.x = (e.pageX - window.innerWidth / 2) / window.innerWidth / ratio;
+        newmouse.y = (e.pageY - window.innerHeight / 2) / window.innerHeight * -1;
+
+        e.preventDefault();
+    });
+
+
+
 }
 
-function onWindowResize( event ) {
-  renderer.setSize( window.innerWidth, window.innerHeight );
-  uniforms.u_resolution.value.x = renderer.domElement.width;
-  uniforms.u_resolution.value.y = renderer.domElement.height;
-  
-  rtTexture = new THREE.WebGLRenderTarget(window.innerWidth * .2, window.innerHeight * .2);
-  rtTexture2 = new THREE.WebGLRenderTarget(window.innerWidth * .2, window.innerHeight * .2);
+function onWindowResize(event) {
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    uniforms.u_resolution.value.x = renderer.domElement.width;
+    uniforms.u_resolution.value.y = renderer.domElement.height;
+
+    rtTexture = new THREE.WebGLRenderTarget(window.innerWidth * .2, window.innerHeight * .2);
+    rtTexture2 = new THREE.WebGLRenderTarget(window.innerWidth * .2, window.innerHeight * .2);
 }
 
 function animate(delta) {
-  requestAnimationFrame( animate );
-  render(delta);
+    requestAnimationFrame(animate);
+    render(delta);
 }
 
 
@@ -101,70 +105,72 @@ function animate(delta) {
 
 
 
-let capturer = new CCapture( { 
-  verbose: true, 
-  framerate: 60,
-  // motionBlurFrames: 4,
-  quality: 90,
-  format: 'webm',
-  workersPath: 'js/'
- } );
+let capturer = new CCapture({
+    verbose: true,
+    framerate: 60,
+    // motionBlurFrames: 4,
+    quality: 90,
+    format: 'webm',
+    workersPath: 'js/'
+});
 let capturing = false;
 
 isCapturing = function(val) {
-  if(val === false && window.capturing === true) {
-    capturer.stop();
-    capturer.save();
-  } else if(val === true && window.capturing === false) {
-    capturer.start();
-  }
-  capturing = val;
+    if (val === false && window.capturing === true) {
+        capturer.stop();
+        capturer.save();
+    } else if (val === true && window.capturing === false) {
+        capturer.start();
+    }
+    capturing = val;
 }
 toggleCapture = function() {
-  isCapturing(!capturing);
+    isCapturing(!capturing);
 }
 
-window.addEventListener('keyup', function(e) { if(e.keyCode == 68) toggleCapture(); });
+window.addEventListener('keyup', function(e) { if (e.keyCode == 68) toggleCapture(); });
 
 let then = 0;
+
 function renderTexture(delta) {
-  // let ov = uniforms.u_buff.value;
-  
-  let odims = uniforms.u_resolution.value.clone();
-  uniforms.u_resolution.value.x = window.innerWidth * .2;
-  uniforms.u_resolution.value.y = window.innerHeight * .2;
+    // let ov = uniforms.u_buff.value;
 
-  uniforms.u_buffer.value = rtTexture2.texture;
-  
-  uniforms.u_renderpass.value = true;
-  
-//   rtTexture = rtTexture2;
-//   rtTexture2 = buffer;
-  
-  window.rtTexture = rtTexture;
-  renderer.setRenderTarget(rtTexture);
-  renderer.render( scene, camera, rtTexture, true );
-  
-  let buffer = rtTexture
-  rtTexture = rtTexture2;
-  rtTexture2 = buffer;
+    let odims = uniforms.u_resolution.value.clone();
+    uniforms.u_resolution.value.x = window.innerWidth * .2;
+    uniforms.u_resolution.value.y = window.innerHeight * .2;
 
-  // uniforms.u_buff.value = ov;
+    uniforms.u_buffer.value = rtTexture2.texture;
 
-  uniforms.u_buffer.value = rtTexture.texture;
-  uniforms.u_resolution.value = odims;
-  uniforms.u_renderpass.value = false;
+    uniforms.u_renderpass.value = true;
+
+    //   rtTexture = rtTexture2;
+    //   rtTexture2 = buffer;
+
+    window.rtTexture = rtTexture;
+    renderer.setRenderTarget(rtTexture);
+    renderer.render(scene, camera, rtTexture, true);
+
+    let buffer = rtTexture
+    rtTexture = rtTexture2;
+    rtTexture2 = buffer;
+
+    // uniforms.u_buff.value = ov;
+
+    uniforms.u_buffer.value = rtTexture.texture;
+    uniforms.u_resolution.value = odims;
+    uniforms.u_renderpass.value = false;
 }
+
 function render(delta) {
-  
-  uniforms.u_mouse.value.x += ( newmouse.x - uniforms.u_mouse.value.x ) * divisor;
-  uniforms.u_mouse.value.y += ( newmouse.y - uniforms.u_mouse.value.y ) * divisor;
-  
-  uniforms.u_time.value = delta * 0.0005;
-  renderer.render( scene, camera );
-  renderTexture();
-  
-  if(capturing) {
-    capturer.capture( renderer.domElement );
-  }
+
+    uniforms.u_mouse.value.x += (newmouse.x - uniforms.u_mouse.value.x) * divisor;
+    uniforms.u_mouse.value.y += (newmouse.y - uniforms.u_mouse.value.y) * divisor;
+
+    uniforms.u_time.value = delta * 0.0005;
+    renderer.render(scene, camera);
+    renderTexture();
+
+    if (capturing) {
+        capturer.capture(renderer.domElement);
+    }
 }
